31c2727656647ebd1b89404a48c6758ae96d3202
Ddtester5 <dd5892631@gmail.com>
2025-01-19 08:30:21 +0000

    add pacages

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Маршрут для проверки сервера
-app.get("/", (req, res) => {
-  res.send("Hello, Express with TypeScript!");
-});
-
-// Запуск сервера
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
