1dfa2099aa6e50e71cf088846b6ac1ffda7df83f
Ddtester5 <dd5892631@gmail.com>
2025-11-17 13:28:56 +0300

    Update rollback_strategy_config.ts

--- src/config/rollback_strategy_config.ts
+++ src/config/rollback_strategy_config.ts
@@ -1,6 +1,6 @@
 export const leverage = 20;
-export const takeProfitRatio = 1.4;
-export const stopLossRatio = 0.7;
+export const takeProfitRatio = 1.8;
+export const stopLossRatio = 0.6;
 export const riskPercentage = 0.005;
 
 export const orderLimit = 20 / riskPercentage / 100;
