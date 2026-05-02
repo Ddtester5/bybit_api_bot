82599cb892797e81ed4fae63677ae80698c42c6a
Ddtester5 <dd5892631@gmail.com>
2026-05-02 18:34:44 +0000

    edit strat

--- src/strategies/strategy.ts
+++ src/strategies/strategy.ts
@@ -1,32 +1,40 @@
 import {
+  STRATEGY_MIN_WEEKLY_GROWTH,
   STRATEGY_RSI_OVERBOUGHT,
   STRATEGY_RSI_PERIOD,
-  STRATEGY_SMA_PERIOD_FAST,
   STRATEGY_SMA_PERIOD_SLOW,
 } from "../config/main_config";
 import { calculateRSI } from "../indicators/rsi";
-import { calculateSMA } from "../indicators/sma";
 import { Candle } from "../types/types";
 
 export function checkSignal(candles: Candle[], index: number): boolean {
-  if (index < STRATEGY_SMA_PERIOD_SLOW || !candles[index]) return false;
+  const weeklyOffset = 24 * 7;
+  if (
+    index < weeklyOffset ||
+    index < STRATEGY_SMA_PERIOD_SLOW ||
+    !candles[index]
+  )
+    return false;
 
   const currentPrice = candles[index].close;
-  const smaSlow = calculateSMA(candles, index, STRATEGY_SMA_PERIOD_SLOW);
-  const smaFast = calculateSMA(candles, index, STRATEGY_SMA_PERIOD_FAST);
+  const priceWeekAgo = candles[index - weeklyOffset].close;
+
+  // 0. Проверка на рост 20% за неделю
+  // (Текущая цена / Цена неделю назад) >= 1.20
+  const isWeeklyGrowth =
+    currentPrice / priceWeekAgo >= STRATEGY_MIN_WEEKLY_GROWTH;
+
+  // const smaSlow = calculateSMA(candles, index, STRATEGY_SMA_PERIOD_SLOW);
+  // const smaFast = calculateSMA(candles, index, STRATEGY_SMA_PERIOD_FAST);
   const rsi = calculateRSI(candles, index, STRATEGY_RSI_PERIOD);
 
-  if (!smaSlow || !smaFast || !rsi) return false;
+  if (!rsi) return false;
 
-  // 1. Глобально падаем (быстрая под медленной)
-  const isGlobalDownTrend = smaFast < smaSlow;
-
-  // 2. Цена находится в зоне между средней и быстрой (коррекция)
-  const isPriceCorrection = currentPrice > smaFast;
-
-  // 3. RSI подтверждает перекупленность на откате
+  // const isGlobalDownTrend = smaFast < smaSlow;
+  // const isPriceCorrection = currentPrice > smaFast;
   const isOverbought = rsi > STRATEGY_RSI_OVERBOUGHT;
 
-  // Сигнал: Тренд вниз, но сейчас случился заброс цены вверх + RSI перегрет
-  return isGlobalDownTrend && isPriceCorrection && isOverbought;
+  // Теперь сигнал сработает только если актив сильно вырос за неделю,
+  // но в моменте начал показывать слабость в рамках нисходящего тренда
+  return isWeeklyGrowth && isOverbought;
 }
