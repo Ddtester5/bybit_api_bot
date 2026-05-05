6cc6209762a6bda6026eafbb7d0d58a6bf7da877
Ddtester5 <dd5892631@gmail.com>
2026-05-05 19:21:15 +0000

    1

--- src/backtest/backtest.ts
+++ src/backtest/backtest.ts
@@ -51,76 +51,78 @@ async function run(params: StrategyParams) {
   return stats;
 }
 
-(async function () {
-  const { symbols, candlesBySymbol, maxLength } = await getInitialData({
-    testMode: true,
-  });
-  const res = new Map<
-    { rsi: number; stop: number; take: number },
-    { pnl: string }
-  >();
-  for (let rsi = 30; rsi <= 70; rsi++) {
-    for (let stop = 0.1; stop <= 0.5; stop += 0.01) {
-      for (let take = 0.1; take <= 0.5; take += 0.01) {
-        console.log(
-          `=== TESTING RSI Overbought = ${rsi} , Stop Loss = ${stop} , Take Profit = ${take}  ===`,
-        );
-        const stats = await run({
-          rsiOverbought: rsi,
-          symbols,
-          candlesBySymbol,
-          maxLength,
-          metrics: false,
-          stopLossPercent: stop,
-          takeProfitPercent: take,
-        });
-        console.log(
-          "pnl : ",
-          stats.pnl.toFixed(2),
-          "\n",
-          "drawdown : ",
-          (stats.maxDrawdown * 100).toFixed(2),
-        );
-        res.set({ rsi, stop, take }, { pnl: stats.pnl.toFixed(2) });
-      }
-    }
-  }
-  console.log("\n\n=== SUMMARY ===");
-
-  const sorted = Array.from(res.entries()).sort(
-    (a, b) => parseFloat(b[1].pnl) - parseFloat(a[1].pnl),
-  );
-
-  console.log("\n--- TOP 5 ---");
-  console.table(
-    sorted.slice(0, 5).map(([params, { pnl }]) => ({
-      rsi: params.rsi,
-      stop: params.stop,
-      take: params.take,
-      pnl,
-    })),
-  );
-
-  console.log("\n--- WORST 5 ---");
-  console.table(
-    sorted.slice(0, 5).map(([params, { pnl }]) => ({
-      rsi: params.rsi,
-      stop: params.stop,
-      take: params.take,
-      pnl,
-    })),
-  );
-})();
-
 // (async function () {
 //   const { symbols, candlesBySymbol, maxLength } = await getInitialData({
 //     testMode: true,
 //   });
-//   run({
-//     rsiOverbought: 70,
-//     symbols,
-//     candlesBySymbol,
-//     maxLength,
-//     metrics: true,
-//   });
+//   const res = new Map<
+//     { rsi: number; stop: number; take: number },
+//     { pnl: string }
+//   >();
+//   for (let rsi = 30; rsi <= 70; rsi += 5) {
+//     for (let stop = 0.1; stop <= 0.3; stop += 0.01) {
+//       for (let take = 0.1; take <= 0.3; take += 0.01) {
+//         console.log(
+//           `=== TESTING RSI Overbought = ${rsi} , Stop Loss = ${stop} , Take Profit = ${take}  ===`,
+//         );
+//         const stats = await run({
+//           rsiOverbought: rsi,
+//           symbols,
+//           candlesBySymbol,
+//           maxLength,
+//           metrics: false,
+//           stopLossPercent: stop,
+//           takeProfitPercent: take,
+//         });
+//         console.log(
+//           "pnl : ",
+//           stats.pnl.toFixed(2),
+//           "\n",
+//           "drawdown : ",
+//           (stats.maxDrawdown * 100).toFixed(2),
+//         );
+//         res.set({ rsi, stop, take }, { pnl: stats.pnl.toFixed(2) });
+//       }
+//     }
+//   }
+//   console.log("\n\n=== SUMMARY ===");
+
+//   const sorted = Array.from(res.entries()).sort(
+//     (a, b) => parseFloat(b[1].pnl) - parseFloat(a[1].pnl),
+//   );
+
+//   console.log("\n--- TOP 10 ---");
+//   console.table(
+//     sorted.slice(0, 10).map(([params, { pnl }]) => ({
+//       rsi: params.rsi,
+//       stop: params.stop,
+//       take: params.take,
+//       pnl,
+//     })),
+//   );
+
+//   console.log("\n--- WORST 5 ---");
+//   console.table(
+//     sorted.slice(0, -5).map(([params, { pnl }]) => ({
+//       rsi: params.rsi,
+//       stop: params.stop,
+//       take: params.take,
+//       pnl,
+//     })),
+//   );
 // })();
+
+(async function () {
+  const { symbols, candlesBySymbol, maxLength } = await getInitialData({
+    testMode: true,
+  });
+  run({
+    stopLossPercent: 0.1,
+    takeProfitPercent: 0.17,
+    rsiOverbought: 40,
+    symbols,
+    candlesBySymbol,
+    maxLength,
+    metrics: true,
+  });
+})();
