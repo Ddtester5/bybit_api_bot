4de910acf0a3fea76e48888013d1df2afd2f93e7
Ddtester5 <dd5892631@gmail.com>
2026-05-16 10:10:52 +0000

    refactoring

--- src/scalping/bybit.ts
+++ src/scalping/bybit.ts
@@ -1,4 +1,4 @@
-import { createClient } from "../../api/bybit_api_client_v5";
+import { createClient } from "../api/bybit_api_client_v5";
 import { config } from "./scalp_config";
 
 export const client = createClient(config.apiKey, config.apiSecret);
